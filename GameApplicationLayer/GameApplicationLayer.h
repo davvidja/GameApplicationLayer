8a0547c7ea9c7275b3474a9b5f2c5973043fcd3f
David Jambrina <david.jambrina@gmail.com>
2014-11-29 12:21:18 +0100

    Initial Commit

---
+++ GameApplicationLayer/GameApplicationLayer.h
@@ -0,0 +1,19 @@
+//
+//  GameApplicationLayer.h
+//  GameApplicationLayer
+//
+//  Created by Carina Macia on 29/11/14.
+//  Copyright (c) 2014 Our company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameApplicationLayer.
+FOUNDATION_EXPORT double GameApplicationLayerVersionNumber;
+
+//! Project version string for GameApplicationLayer.
+FOUNDATION_EXPORT const unsigned char GameApplicationLayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameApplicationLayer/PublicHeader.h>
+
+
